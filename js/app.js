17a103108739accafd584b7f084e1a7a4cc56e85
ARBUCHELI <radio_seattle@hotmail.com>
2023-06-04 19:58:58 +0300

    refactoring the final solution of the project

--- js/app.js
+++ js/app.js
@@ -24,13 +24,14 @@
 */
 // Wait for the DOM content to load
 document.addEventListener('DOMContentLoaded', () => {
-  // Get the navbar list element and define variables
+  // Geting the navbar list element, the section, and defining variables
   const navbarList = document.getElementById('navbar__list');
   const sections = document.querySelectorAll('section');
   const fragment = document.createDocumentFragment();
   
   /**
    * End Variables
+   * Iterating over the sections using a forEach method for building the nav
    */
   sections.forEach((section) => {
     const sectionId = section.id;
@@ -43,7 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
     anchor.classList.add('menu__link');
     anchor.href = `#${sectionId}`;
     anchor.textContent = sectionNav;
-  
+    
+    // Creating functionality for scrolling to the selected section when clicking the nav items.
     anchor.addEventListener('click', (event) => {
       event.preventDefault();
       // Scroll to section on link click
@@ -58,12 +60,14 @@ document.addEventListener('DOMContentLoaded', () => {
     fragment.appendChild(listItem);
   });
   
+  // Creating an extra nav item used for scrolling back to the top of the page.
   const backToTopItem = document.createElement('li');
   const backToTopAnchor = document.createElement('a');
   backToTopAnchor.classList.add('menu__link');
   backToTopAnchor.href = '#top';
   backToTopAnchor.textContent = 'Back to Top';
   
+  // Adding functionality for scrolling back to the top of the page.
   backToTopAnchor.addEventListener('click', (event) => {
     event.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -76,6 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
   navbarList.appendChild(fragment);
 
   // Helper functions
+  // Function used for adding the "activated-section" to the sections.
   const activateSection = (sectionId) => {
     sections.forEach((section) => {
       section.classList.remove('activated-section');
@@ -85,6 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
     selectedSection.classList.add('activated-section');
   };
 
+  // Function used for adding the "activated-nav" to the nav items.
   const activateNav = (clickedNav) => {
     const navItems = document.querySelectorAll('.menu__link');
     navItems.forEach((navItem) => {
@@ -94,6 +100,10 @@ document.addEventListener('DOMContentLoaded', () => {
     clickedNav.classList.add('activated-nav');
   };
 
+  /** 
+   * In order to implement the functionality of adding the "activated-section" class, whewn scrolling, I found a solution that 
+   * I consider a little bit simpler, and I used two different built-in Web API methods (offseTop, and offsetHeight)
+   */
   window.addEventListener('scroll', () => {
     // Get the current position of the scrollbar
     const scrollPosition = window.scrollY + 10;
